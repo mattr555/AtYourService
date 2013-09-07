8ae83af43071f58a01e6515a1e2fa5a4a09bfa62
Matthew Ramina <splattkid@gmail.com>
2013-09-06 22:51:47 -0400

    Added some database indexes added logging for sql queries in dev settings

--- AtYourService/settings.py
+++ AtYourService/settings.py
@@ -110,35 +110,6 @@ INSTALLED_APPS = (
     'bootstrapform',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 EMAIL_SUBJECT_PREFIX = '[AtYourService] '
 
 TEMPLATE_CONTEXT_PROCESSORS = ('django.contrib.auth.context_processors.auth',
