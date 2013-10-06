6bb4b24d39d04775730b2e9eaa6f6ad81f08a57e
Matthew Ramina <mattramina@gmail.com>
2013-10-06 12:44:27 -0400

    Made email_tasks check validation

--- main/management/commands/email_tasks.py
+++ main/management/commands/email_tasks.py
@@ -26,7 +26,7 @@ class Command(NoArgsCommand):
         for i in set:
             plaintext = render_to_string('email/event_remind.txt', {'site': site, 'event': i})
             html = render_to_string('email/event_remind.html', {'site': site, 'event': i})
-            recipients = list(map(lambda x: x.email, i.participants.all()))
+            recipients = list(map(lambda x: x.email, i.participants.filter(user_profile__email_valid=True)))
             send_html_mail('Event reminder for {}'.format(i.name),
                            plaintext,
                            html,
